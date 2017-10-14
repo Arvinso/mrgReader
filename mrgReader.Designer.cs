a6dd59c5f95b3082ca465d32a7ef8849575c9e34
vsoedhwa <vsoedhwa@sfu.ca>
2017-10-14 12:29:31 -0700

    for archiving

---
+++ mrgReader.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace mrgReader
+{
+    partial class mrgReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DecompressedResultBox = new System.Windows.Forms.PictureBox();
+            this.OpenMrgFile_btn = new System.Windows.Forms.Button();
+            this.Quality_found = new System.Windows.Forms.TextBox();
+            this.width_found = new System.Windows.Forms.TextBox();
+            this.height_found = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Matrixviewer = new System.Windows.Forms.DataGridView();
+            this.Column0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DecompressedResultBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Matrixviewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DecompressedResultBox
+            // 
+            this.DecompressedResultBox.Location = new System.Drawing.Point(167, 60);
+            this.DecompressedResultBox.Name = "DecompressedResultBox";
+            this.DecompressedResultBox.Size = new System.Drawing.Size(369, 311);
+            this.DecompressedResultBox.TabIndex = 0;
+            this.DecompressedResultBox.TabStop = false;
+            // 
+            // OpenMrgFile_btn
+            // 
+            this.OpenMrgFile_btn.Location = new System.Drawing.Point(254, 12);
+            this.OpenMrgFile_btn.Name = "OpenMrgFile_btn";
+            this.OpenMrgFile_btn.Size = new System.Drawing.Size(176, 42);
+            this.OpenMrgFile_btn.TabIndex = 1;
+            this.OpenMrgFile_btn.Text = "Open mrg file";
+            this.OpenMrgFile_btn.UseVisualStyleBackColor = true;
+            this.OpenMrgFile_btn.Click += new System.EventHandler(this.OpenMrgFile_btn_Click);
+            // 
+            // Quality_found
+            // 
+            this.Quality_found.Location = new System.Drawing.Point(149, 409);
+            this.Quality_found.Name = "Quality_found";
+            this.Quality_found.Size = new System.Drawing.Size(100, 22);
+            this.Quality_found.TabIndex = 2;
+            // 
+            // width_found
+            // 
+            this.width_found.Location = new System.Drawing.Point(149, 437);
+            this.width_found.Name = "width_found";
+            this.width_found.Size = new System.Drawing.Size(100, 22);
+            this.width_found.TabIndex = 3;
+            // 
+            // height_found
+            // 
+            this.height_found.Location = new System.Drawing.Point(149, 465);
+            this.height_found.Name = "height_found";
+            this.height_found.Size = new System.Drawing.Size(100, 22);
+            this.height_found.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Quality Detected";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 442);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 470);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "height";
+            // 
+            // Matrixviewer
+            // 
+            this.Matrixviewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Matrixviewer.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column0,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.Matrixviewer.Location = new System.Drawing.Point(731, 36);
+            this.Matrixviewer.Name = "Matrixviewer";
+            this.Matrixviewer.RowTemplate.Height = 24;
+            this.Matrixviewer.Size = new System.Drawing.Size(688, 395);
+            this.Matrixviewer.TabIndex = 75;
+            // 
+            // Column0
+            // 
+            this.Column0.HeaderText = "Col0";
+            this.Column0.Name = "Column0";
+            this.Column0.Width = 50;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Col1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Col2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 50;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Col3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 50;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Col4";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 50;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Col5";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 50;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Col6";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 50;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Col7";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 50;
+            // 
+            // mrgReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1444, 527);
+            this.Controls.Add(this.Matrixviewer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.height_found);
+            this.Controls.Add(this.width_found);
+            this.Controls.Add(this.Quality_found);
+            this.Controls.Add(this.OpenMrgFile_btn);
+            this.Controls.Add(this.DecompressedResultBox);
+            this.Name = "mrgReader";
+            this.Text = "mrgReader";
+            ((System.ComponentModel.ISupportInitialize)(this.DecompressedResultBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Matrixviewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DecompressedResultBox;
+        private System.Windows.Forms.Button OpenMrgFile_btn;
+        private System.Windows.Forms.TextBox Quality_found;
+        private System.Windows.Forms.TextBox width_found;
+        private System.Windows.Forms.TextBox height_found;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView Matrixviewer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column0;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+    }
+}
+
---
+++ mrgReader.cs
@@ -0,0 +1,508 @@
+﻿using Accord.Imaging;
+using Accord.Math;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace mrgReader
+{   
+
+    public struct MRGfile
+    {
+        public int width;
+        public int height;
+
+        public List<int> zigzagY;
+        public List<int> zigzagU;
+        public List<int> zigzagV;
+
+        public int quality;
+
+        public MRGfile(List<int> Y, List<int> U, List<int> V, int Quality, int Width, int Height)
+        {
+            this.zigzagY = Y;
+            this.zigzagU = U;
+            this.zigzagV = V;
+            this.quality = Quality;
+            this.width = Width;
+            this.height = Height;
+        }
+    }
+
+
+    struct Yuv
+    {
+        public double Yf;      //Conversion from RGB
+        public double Uf;      //Conversion from RGB
+        public double Vf;      //Conversion from RGB
+
+        public int Ynorm;           //normalized values
+        public int Unorm;           //normalized values
+        public int Vnorm;           //normalized values
+
+        public double Ydct;      //DCT values
+        public double Udct;      //DCT values
+        public double Vdct;      //DCT values
+
+        public int Yquant;        //After quantization
+        public int Uquant;        //After quantization
+        public int Vquant;        //After quantization
+
+    }
+
+
+
+
+
+
+    struct myYcBc
+    {
+        public int width;
+        public int height;
+
+        public Yuv[,] ycbcrArr;
+
+        public List<int> zigzagY;
+        public List<int> zigzagU;
+        public List<int> zigzagV;
+
+        public myYcBc(int width, int height)
+        {
+            //Y = new byte[width, height];
+            //Cb = new byte[width, height];
+            //Cr = new byte[width, height];
+
+            ycbcrArr = new Yuv[width, height];
+            zigzagY = new List<int>();
+            zigzagU = new List<int>();
+            zigzagV = new List<int>();
+
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+
+
+
+
+    public struct Coords
+    {
+        public int xvalue;
+        public int yvalue;
+
+        public Coords(int x, int y) : this()
+        {
+            this.xvalue = x;
+            this.yvalue = y;
+        }
+    }
+
+
+
+
+
+    public partial class mrgReader : Form
+    {
+        public int[,] QuantMatrix = new int[8, 8] { 
+                                          { 16,11,10,16,24,40,51,61 },
+                                          { 12,12,14,19,26,58,60,55 },
+                                          { 14,13,16,24,40,57,69,56 },
+                                          { 14,17,22,29,51,87,80,62 },
+                                          { 18,22,37,56,68,109,103,77 },
+                                          { 24,35,55,64,81,104,113,92 },
+                                          { 49,64,78,87,103,121,120,101 },
+                                          { 72,92,95,98,112,100,103,99 },
+                                        };
+
+        public mrgReader()
+        {
+            InitializeComponent();
+            for (int rows = 0; rows < 8; rows++)
+            Matrixviewer.Rows.Add();
+        }
+
+
+        public bool ThumbnailCallback()
+        {
+            return false;
+        }
+
+
+        private void OpenMrgFile_btn_Click(object sender, EventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select a Cursor.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "MRG Files(*.MRG)|*.MRG|All files (*.*)|*.*";
+            openFileDialog1.Title = "Select an MRG File";
+
+            // Show the Dialog.
+
+
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (FileStream originalFileStream = (FileStream)openFileDialog1.OpenFile())
+                {
+                    using (FileStream decompressedFileStream = File.Create("temp"))
+                    {
+                        using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                        {
+                            decompressionStream.CopyTo(decompressedFileStream);
+                        }
+                    }
+                }
+            }
+
+            int Quality = 0;
+            int width = 0;
+            int height = 0;
+
+            string line;
+
+            StreamReader file = new StreamReader("temp");
+
+            if ((line = file.ReadLine()) != null)
+            {
+                int x = 0;
+                int.TryParse(line, out x);
+                Quality = x;
+            }
+
+            if ((line = file.ReadLine()) != null)
+            {
+                int x = 0;
+                int.TryParse(line, out x);
+                width = x;
+            }
+            if ((line = file.ReadLine()) != null)
+            {
+                int x = 0;
+                int.TryParse(line, out x);
+                height = x;
+            }
+
+            List<int> zigzagY = new List<int>();
+            List<int> zigzagU = new List<int>();
+            List<int> zigzagV = new List<int>();
+
+            for (int i = 0; i < (width * height); i++)
+            {
+                if ((line = file.ReadLine()) != null)
+                {
+                    int x = 0;
+                    int.TryParse(line, out x);
+                    zigzagY.Add(x);
+                }
+            }
+
+            for (int i = 0; i < (width * height); i++)
+            {
+                if ((line = file.ReadLine()) != null)
+                {
+                    int x = 0;
+                    int.TryParse(line, out x);
+                    zigzagU.Add(x);
+                }
+            }
+
+            for (int i = 0; i < (width * height); i++)
+            {
+                if ((line = file.ReadLine()) != null)
+                {
+                    int x = 0;
+                    int.TryParse(line, out x);
+                    zigzagV.Add(x);
+                }
+            }
+
+
+            Quality_found.Text = Quality.ToString();
+            width_found.Text = width.ToString();
+            height_found.Text = height.ToString();
+
+            //Create mrgfile object to work with
+
+            MRGfile mrgfile = new MRGfile(zigzagY, zigzagU, zigzagV, Quality, width, height);
+
+            QualityMatrixAlter(Quality);
+
+
+            //////////
+            /////////////// convert ZigZag format to array 
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            myYcBc MyOwnYcbc = new myYcBc(width, height);
+
+
+            int dezigcount = 0;
+
+            for (int y = 0; y < mrgfile.height; y += 8)
+            {
+                for (int x = 0; x < mrgfile.width; x += 8)
+                {            
+
+                    for (int index = 0; index < 64; index++)
+                    {                       
+                        Coords Targetcoords;
+
+                        Targetcoords = ZZStraverseValue(index);
+
+                        MyOwnYcbc.ycbcrArr[Targetcoords.xvalue + x, Targetcoords.yvalue + y].Yquant = mrgfile.zigzagY[dezigcount];
+                        MyOwnYcbc.ycbcrArr[Targetcoords.xvalue + x, Targetcoords.yvalue + y].Uquant = mrgfile.zigzagU[dezigcount];
+                        MyOwnYcbc.ycbcrArr[Targetcoords.xvalue + x, Targetcoords.yvalue + y].Vquant = mrgfile.zigzagV[dezigcount];
+
+                        dezigcount++;                        
+                    }
+                }
+            }
+
+
+            //////////
+            /////////////// Reverse Quantization
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            for (int y = 0; y < MyOwnYcbc.height; y += 8)
+            {
+                for (int x = 0; x < MyOwnYcbc.width; x += 8)
+                {
+                    for (int ity = y; ity < y + 8; ity++)
+                    {
+                        for (int itx = x; itx < x + 8; itx++)
+                        {
+                            if (itx < MyOwnYcbc.width && ity < MyOwnYcbc.height)
+                            {
+                                MyOwnYcbc.ycbcrArr[itx, ity].Ydct = MyOwnYcbc.ycbcrArr[itx, ity].Yquant  * QuantMatrix[itx - x, ity - y];
+                                MyOwnYcbc.ycbcrArr[itx, ity].Udct = MyOwnYcbc.ycbcrArr[itx, ity].Uquant  * QuantMatrix[itx - x, ity - y];
+                                MyOwnYcbc.ycbcrArr[itx, ity].Vdct = MyOwnYcbc.ycbcrArr[itx, ity].Vquant  * QuantMatrix[itx - x, ity - y];
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            //////////
+            /////////////// Inverse DCT
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+            double[,] YtempIDCT = new double[8, 8];
+            double[,] UtempIDCT = new double[8, 8];
+            double[,] VtempIDCT = new double[8, 8];
+
+
+            for (int y = 0; y < MyOwnYcbc.height; y += 8)
+            {
+                for (int x = 0; x < MyOwnYcbc.width; x += 8)
+                {
+                    if (x <= MyOwnYcbc.width - 8 && y <= MyOwnYcbc.height - 8) //safeguard against out of bounds error
+                    {
+
+                        for (int ity = y; ity < y + 8; ity++)
+                        {
+                            for (int itx = x; itx < x + 8; itx++)
+                            {
+                                YtempIDCT[ity - y, itx - x] = MyOwnYcbc.ycbcrArr[itx, ity].Ydct;
+                                UtempIDCT[ity - y, itx - x] = MyOwnYcbc.ycbcrArr[itx, ity].Udct;
+                                VtempIDCT[ity - y, itx - x] = MyOwnYcbc.ycbcrArr[itx, ity].Vdct;
+                            }
+                        }
+
+                        CosineTransform.IDCT(YtempIDCT);
+                        CosineTransform.IDCT(UtempIDCT);
+                        CosineTransform.IDCT(VtempIDCT);
+
+                        for (int ity = y; ity < y + 8; ity++)
+                        {
+                            for (int itx = x; itx < x + 8; itx++)
+                            {
+                                MyOwnYcbc.ycbcrArr[itx, ity].Ynorm = (int)YtempIDCT[ity - y, itx - x];
+                                MyOwnYcbc.ycbcrArr[itx, ity].Unorm = (int)UtempIDCT[ity - y, itx - x];
+                                MyOwnYcbc.ycbcrArr[itx, ity].Vnorm = (int)VtempIDCT[ity - y, itx - x];
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            //////////
+            /////////////// DEnormalize from -128 <-> 127 range to 0 <-> 255
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            for (int y = 0; y < MyOwnYcbc.height; y++)
+            {
+                for (int x = 0; x < MyOwnYcbc.width; x++)
+                {
+                    Yuv ycColorspaceEdit = MyOwnYcbc.ycbcrArr[x, y];
+
+                    ycColorspaceEdit.Yf =   (ycColorspaceEdit.Ynorm) + 128;
+
+                    if (y % 2 == 0 && x % 2 == 0)
+                    {
+                        ycColorspaceEdit.Uf = (ycColorspaceEdit.Unorm) + 128;
+                        ycColorspaceEdit.Vf = (ycColorspaceEdit.Vnorm) + 128;
+                    }
+                    else
+                    {
+                        ycColorspaceEdit.Uf = (ycColorspaceEdit.Unorm);
+                        ycColorspaceEdit.Vf = (ycColorspaceEdit.Vnorm);
+                    }
+
+                    MyOwnYcbc.ycbcrArr[x, y] = ycColorspaceEdit;
+                }
+            }
+
+
+            //////////
+            /////////////// Convert from YUV to RGB and assign to bitmap image
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            Bitmap Finalbitmap = new Bitmap(MyOwnYcbc.width,MyOwnYcbc.height);
+
+            for (int y = 0; y < MyOwnYcbc.height; y++)
+            {
+                for (int x = 0; x < MyOwnYcbc.width; x++)
+                {                    
+                    YCbCr ycrcb = new YCbCr();
+                    RGB rgbcolor = new RGB();
+                    
+                    ycrcb.Y =  (float)(MyOwnYcbc.ycbcrArr[x, y].Yf /255);
+                    ycrcb.Cr = (float)(MyOwnYcbc.ycbcrArr[x, y].Uf /255);
+                    ycrcb.Cb = (float)(MyOwnYcbc.ycbcrArr[x, y].Vf /255);
+
+                   //rgbcolor.Red = (byte)(ycrcb.Cr * 255);
+                   //rgbcolor.Blue = (byte)(ycrcb.Cr * 255);
+                   //rgbcolor.Green = (byte)(ycrcb.Cr * 255);
+                    YCbCr.ToRGB(ycrcb, rgbcolor);
+
+                    Finalbitmap.SetPixel(x, y, rgbcolor.Color);                    
+                }
+            }
+
+
+            System.Drawing.Image.GetThumbnailImageAbort myCallback = new System.Drawing.Image.GetThumbnailImageAbort(ThumbnailCallback);
+
+
+            DecompressedResultBox.Image = Finalbitmap.GetThumbnailImage(DecompressedResultBox.Width, DecompressedResultBox.Height, myCallback, IntPtr.Zero);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Coords testcoords;
+
+            testcoords.xvalue = 920;
+            testcoords.yvalue = 0;
+
+            for (int y = testcoords.yvalue; y < testcoords.yvalue + 8; y++)
+            {
+                for (int x = testcoords.xvalue; x < testcoords.xvalue + 8; x++)
+                {
+                    Matrixviewer.Rows[y - testcoords.yvalue].Cells[x - testcoords.xvalue].Value = MyOwnYcbc.ycbcrArr[x, y].Yquant;
+                    //Matrixviewer.Rows[x].Cells[y].Value = QuantMatrix[x, y];
+                    //Matrixviewer.Rows[x].Cells[y].Value = testoutputmatrix[x, y];
+                }
+            }
+
+
+            //for (int y = 0; y < 8; y++)
+            //{
+            //    for (int x = 0; x <  8; x++)
+            //    {
+            //        Matrixviewer.Rows[x].Cells[y].Value = QuantMatrix[x, y];
+            //        //Matrixviewer.Rows[x].Cells[y].Value = testoutputmatrix[x, y];
+            //    }
+            //}
+
+
+
+
+
+
+
+
+
+
+
+        }       
+
+
+        public Coords ZZStraverseValue(int index)
+        {
+            Coords[] ZigZagLookupTable = new Coords[64]{
+             new Coords(0,0),new Coords(1,0),new Coords(0,1),new Coords(0,2),new Coords(1,1),new Coords(2,0),new Coords(3,0),new Coords(2,1), 
+             new Coords(1,2),new Coords(0,3),new Coords(0,4),new Coords(1,3),new Coords(2,2),new Coords(3,1),new Coords(4,0),new Coords(5,0), 
+             new Coords(4,1),new Coords(3,2),new Coords(2,3),new Coords(1,4),new Coords(0,5),new Coords(0,6),new Coords(1,5),new Coords(2,4), 
+             new Coords(3,3),new Coords(4,2),new Coords(5,1),new Coords(6,0),new Coords(7,0),new Coords(6,1),new Coords(5,2),new Coords(4,3), 
+             new Coords(3,4),new Coords(2,5),new Coords(1,6),new Coords(0,7),new Coords(1,7),new Coords(2,6),new Coords(3,5),new Coords(4,4), 
+             new Coords(5,3),new Coords(6,2),new Coords(7,1),new Coords(7,2),new Coords(6,3),new Coords(5,4),new Coords(4,5),new Coords(3,6), 
+             new Coords(2,7),new Coords(3,7),new Coords(4,6),new Coords(5,5),new Coords(6,4),new Coords(7,3),new Coords(7,4),new Coords(6,5), 
+             new Coords(5,6),new Coords(4,7),new Coords(5,7),new Coords(6,6),new Coords(7,5),new Coords(7,6),new Coords(6,7),new Coords(7,7) 
+            };
+
+            return ZigZagLookupTable[index];
+        }
+
+        public void QualityMatrixAlter(int QuantizationQuality)
+        {
+            if (QuantizationQuality != 50)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    for (int y = 0; y < 8; y++)
+                    {
+                        if (QuantizationQuality > 50)
+                        {
+                            QuantMatrix[x, y] = (int)(QuantMatrix[x, y] * ((float)(100 - QuantizationQuality) / 50f));
+                            if (QuantMatrix[x, y] > 255)
+                            {
+                                QuantMatrix[x, y] = 255;
+                            }
+                            if (QuantMatrix[x, y] == 0)
+                            {
+                                QuantMatrix[x, y] = 1;
+                            }
+                        }
+                        else if (QuantizationQuality < 50)
+                        {
+                            QuantMatrix[x, y] = QuantMatrix[x, y] * (50 / QuantizationQuality);
+                            if (QuantMatrix[x, y] > 255)
+                            {
+                                QuantMatrix[x, y] = 255;
+                            }
+                            else if (QuantMatrix[x, y] == 0)
+                            {
+                                QuantMatrix[x, y] = 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
